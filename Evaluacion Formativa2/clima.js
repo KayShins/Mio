96137e1eaecb67bf050a7a9a5e44fe2ab106b2e4
Lesther Araya <163434091+KayShins@users.noreply.github.com>
2024-05-11 14:30:48 -0400

    Actualizar clima.js

--- Evaluacion Formativa2/clima.js
+++ Evaluacion Formativa2/clima.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
         $.get("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Punta%20Arenas?unitGroup=metric&key=X9LMSEH7VBFLWJ7L65WCK6E3H&contentType=json&days=10",
             function(data){
                 $.each(data.days,function(i,item){
-                    $('days').append("<tr><td>"+item.datetime + "<tr><td>" + item.tempmax + "<tr><td>" + item.tempmin + "<tr><td>" +item.preciptype)
+                    $('dias').append("<tr><td>"+item.datetime + "<tr><td>" + item.tempmax + "<tr><td>" + item.tempmin + "<tr><td>" +item.preciptype)
                 }
             )
             });
